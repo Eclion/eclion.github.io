ff86d2d04a297a569df2ea415f41cac9c26a7376
eclion <eclionast@gmail.com>
2026-05-21 12:00:11 +0200

    refacto: moves exps to dedicated js file,  use it for templating

---
+++ experiences.js
@@ -0,0 +1,14 @@
+const experienceMap = {
+    senior: {
+        title: 'Lorem Ipsum',
+        subtitle: 'Tech Company • 2022 - Present',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    },
+    frontend: {
+        title: 'Lorem Ipsum',
+        subtitle: 'Startup Inc • 2020 - 2022',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    }
+};
